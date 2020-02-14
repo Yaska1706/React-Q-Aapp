29b5bd2823869a357ea2b160e496f3f21a94aedf
yaska1706 <yasinkinyua@gmail.com>
2020-02-14 04:28:10 +0300

    First commit

---
+++ user/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import NavBar from './NavBar/NavBar';
+import Question from './Question/Question';
+import Questions from './Questions/Questions';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
+      </div>
+    );
+  }
+}
+
+export default App;
