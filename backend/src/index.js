16fe9d1e3895710cceefeb5e78dbd93ce8ecb5c0
yaska1706 <yasinkinyua@gmail.com>
2020-02-27 03:02:04 +0300

    Added Auth authentification

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,10 @@ app.use(cors());
 // log HTTP requests
 app.use(morgan('combined'));
 
+// ... other require statements ...
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 // retrieve all questions
 app.get('/', (req, res) => {
   const qs = questions.map(q => ({
@@ -41,22 +45,44 @@ app.get('/:id', (req, res) => {
   if (question.length === 0) return res.status(404).send();
   res.send(question[0]);
 });
+// ... require statements ...
 
+// ... app definitions ...
+
+// ... app.get endpoints ...
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://yaska-1706.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: '<YYkgLsyqUk8AfFUR98rN683lboYZX4Do>',
+  issuer: `https://yaska-1706.auth0.com/`,
+  algorithms: ['RS256']
+});
+
+// ... app.post endpoints ...
+
+// ... app.listen ...
 // insert a new question
-app.post('/', (req, res) => {
+app.post('/', checkJwt, (req, res) => {
   const {title, description} = req.body;
   const newQuestion = {
     id: questions.length + 1,
     title,
     description,
     answers: [],
+    author: req.user.name,
   };
   questions.push(newQuestion);
   res.status(200).send();
 });
-
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
+app.post('/answer/:id', checkJwt, (req, res) => {
   const {answer} = req.body;
 
   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
@@ -65,12 +91,13 @@ app.post('/answer/:id', (req, res) => {
 
   question[0].answers.push({
     answer,
+    author: req.user.name,
   });
 
   res.status(200).send();
 });
-
 // start the server
 app.listen(8081, () => {
   console.log('listening on port 8081');
 });
+
--- user/src/App.js
+++ user/src/App.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Question from './Question/Question';
 import Questions from './Questions/Questions';
+// ... other import statements ...
+import Callback from './Callback';
+
 
 class App extends Component {
   render() {
@@ -10,6 +14,7 @@ class App extends Component {
         <NavBar/>
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
+        <Route exact path='/callback' component={Callback}/>
       </div>
     );
   }
